4d222145cc8ec2d05c0f78ac3d8c62f74f7f106b
Luisa Negri <luisaanegri@gmail.com>
2018-07-05 12:17:59 +0000

    connected flask to mongodb - created first route

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 import os
+from flask_pymongo import PyMongo
+
 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'cookbook_app'
+app.config["MONGO_URI"] = 'mongodb://root:adminr00t@ds227481.mlab.com:27481/cookbook_app'
 
-@app.route("/")
-def index():
-    return render_template('index.html')
-
-
+mongo = PyMongo(app)
 
 
 
-
-
-
+@app.route("/get_recipes")
+def get_recipes():
+    return render_template('recipes.html', recipes=mongo.db.recipes.find())
+    
 
 
 
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
+        app.run(host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=True)
     
