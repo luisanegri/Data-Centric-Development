d3202066cbca6bbb7ec9b69243917bd8c0ccd5f8
Luisa Negri <luisaanegri@gmail.com>
2018-07-06 13:10:55 +0000

    created new function add_recipes

--- app.py
+++ app.py
@@ -17,7 +17,11 @@ def get_recipes():
     return render_template('recipes.html', recipes=mongo.db.recipes.find())
     
 
-
+@app.route("/add_recipes")
+def add_recipes():
+    return render_template('addrecipes.html', categories=mongo.db.categories.find(), 
+    difficulty=mongo.db.difficulty.find())
+    
 
 
 
