27418eafb141d2c3dc4e7c90ee1c556739844572
Luisa Negri <luisaanegri@gmail.com>
2018-07-06 15:04:41 +0000

    function/form to register username

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 import os
 from flask_pymongo import PyMongo
+import bcrypt
 
 
 
@@ -11,6 +12,21 @@ app.config["MONGO_URI"] = 'mongodb://root:adminr00t@ds227481.mlab.com:27481/cook
 mongo = PyMongo(app)
 
 
+@app.route("/register", methods=['POST','GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        
+        return 'That username already exists!'
+
+    return render_template('register.html')
 
 @app.route("/get_recipes")
 def get_recipes():
@@ -34,6 +50,7 @@ def insert_recipe():
 
 
 if __name__ == '__main__':
+        app.secret_key = 'mysecret'
         app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
         debug=True)
